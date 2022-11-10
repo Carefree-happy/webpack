7c7eda75a0bfe134d26a14aa2401a4b45a3b5ee9
sunjing <sunjing@mzhcorp.com>
2022-11-10 23:32:52 +0800

    feat: webpack Loader

--- webpack.config.js
+++ webpack.config.js
@@ -7,4 +7,12 @@ module.exports = {
         filename: "bundle.js", // 输出文件名
         path: path.join(__dirname, "dist"), // 输出文件目录
     },
+    module: {
+        rules: [    // 转换规则
+            {
+                test: /\.css$/, // 匹配所有的 css 文件
+                use: 'css-loader' // use: 对应的 Loader 名称
+            }
+        ]
+    }
 };
